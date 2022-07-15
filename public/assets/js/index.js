b553e8ae5447ca282683eb54e90ee135856b08c4
Angie <103882157+anakela@users.noreply.github.com>
2022-07-14 19:18:30 -1000

    Added routes folders.

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,7 +1,7 @@
-// const express = require('express');
-// const fs = require('fs');
+const express = require('express');
+const fs = require('fs');
 
-// const app = express();
+const app = express();
 
 let noteTitle;
 let noteText;
---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    
+});
+
+router.post('/notes', (req, res) => {
+
+});
+
+router.delete('/notes/:id', (req, res) => {
+
+});
+
+module.exports = router;
